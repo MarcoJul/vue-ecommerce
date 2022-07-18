b404e576b02da65d33c336d51ed77a143885c52f
marco <marco.giuliotti@gmail.com>
2022-07-18 16:57:40 +0200

    add script to collection page

--- src/components/collectionPage/CollectionsPage.vue
+++ src/components/collectionPage/CollectionsPage.vue
@@ -3,5 +3,42 @@
 </template>
 
 <script>
-export default {};
+import CollectionItem from "./CollectionItem.vue";
+export default {
+   components: { CollectionItem },
+   data() {
+      return {
+         collections: [],
+      };
+   },
+   methods: {
+      loadCollections() {
+         fetch("https://4ilk3v7wbk.execute-api.eu-west-1.amazonaws.com/dev/collection_listings.json")
+            .then((response) => {
+               if (response.ok) {
+                  return response.json();
+               }
+            })
+            .then((data) => {
+               const collections = data.collection_listings;
+               const results = [];
+               for (const collection_id in collections) {
+                  results.push({
+                     id: collections[collection_id].collection_id,
+                     handle: collections[collection_id].handle,
+                     title: collections[collection_id].title,
+                     image: collections[collection_id].default_product_image.src,
+                  });
+               }
+               this.collections = results;
+            })
+            .catch((err) => {
+               console.log(err);
+            });
+      },
+   },
+   mounted() {
+      this.loadCollections();
+   },
+};
 </script>
