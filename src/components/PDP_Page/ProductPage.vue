e30db27791e60ddc66d7dd53aba0460f2e57e0d5
marco <marco.giuliotti@gmail.com>
2022-07-19 11:56:32 +0200

    add connection with product page

---
+++ src/components/PDP_Page/ProductPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Product Page</h1>
+</template>
+
+<script>
+export default {
+  computed: {
+    route() {
+      return this.$route.params.id;
+    },
+  },
+  watch: {
+    route: {
+      handler(value) {
+        console.log(value);
+      },
+      immediate: true,
+    },
+  },
+};
+</script>
