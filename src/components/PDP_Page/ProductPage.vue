a5c4bd11c2e14c76b24cc3790bb364cd8243b26d
marco <marco.giuliotti@gmail.com>
2022-07-19 12:18:42 +0200

    add fetch product

--- src/components/PDP_Page/ProductPage.vue
+++ src/components/PDP_Page/ProductPage.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>Product Page</h1>
+  <h1>{{ product.title }}</h1>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      product: {},
+    };
+  },
   computed: {
     route() {
       return this.$route.params.id;
@@ -13,8 +18,24 @@ export default {
     route: {
       handler(value) {
         console.log(value);
+        this.getProduct(value);
       },
       immediate: true,
+    },
+  },
+  methods: {
+    async getProduct(productId) {
+      if (!productId) return;
+      try {
+        const response = await fetch(
+          `https://4ilk3v7wbk.execute-api.eu-west-1.amazonaws.com/dev/products/${productId}.json`
+        );
+        const data = await response.json();
+        this.product = data.product;
+        console.log("product", this.product);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
